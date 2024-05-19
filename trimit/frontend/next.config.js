e0e1827598ae3ff9d21af120fc8066b3784bcafd
Ben Schreck <ben@benschreck.com>
2024-05-19 14:02:04 -0700

    try other var

--- trimit/frontend/next.config.js
+++ trimit/frontend/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/:path*"
-            : `${process.env.NODE_ENV}/api/`,
+            : `${process.env.TEST_ENV}/api/`,
       },
     ];
   },
